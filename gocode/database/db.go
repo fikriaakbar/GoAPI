560facf33b8b0b1235dc62538f692fa802c6368d
unknown <fikriakbarhsb@gmail.com>
2020-09-14 20:48:28 +0700

    first commit

--- gocode/database/db.go
+++ gocode/database/db.go
@@ -43,20 +43,19 @@ func InsertDb(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	stmt, err := db.Prepare("insert into DATA_BARANG(NAMA_BARANG, JUMLAH_BARANG, TANGGAL_DIMASUKAN, TANGGAL_UPDATE) VALUES(:1, :2, :3, :4)") 
-	if err != nil { 
-    fmt.Println(err) 
-    return 
+	stmt, err := db.Prepare("insert into DATA_BARANG(NAMA_BARANG, JUMLAH_BARANG, TANGGAL_DIMASUKAN, TANGGAL_UPDATE) VALUES(:1, :2, :3, :4)")
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 
-	res, err := db.Query("AIR JORDAN 4", 10, "NOW()", "NOW()")  
-	if err != nil { 
-    fmt.Println(".....Error Inserting data") 
-    fmt.Println(err) 
-    return  
-	} 
+	res, err := db.Query("AIR JORDAN 4", 10, "NOW()", "NOW()")
+	if err != nil {
+		fmt.Println(".....Error Inserting data")
+		fmt.Println(err)
+		return
+	}
 
 	rowCnt := res.RowsAffected()
 	fmt.Println(rowCnt, "rows inserted.")
 }
-
